6a1ff2b918923ae4f3a9a82c775b5b76e7a78dc9
mparaiso <aikah@free.fr>
2015-04-20 22:02:46 +0200

    Update pimple.go

--- pimple.go
+++ pimple.go
@@ -1,10 +1,31 @@
 package pimple
 
+import "sync"
+
 type Pimple struct{
-  services map[string]func(pimple Pimple)interface{}
+  services map[string]func(*Pimple)interface{}
 }
 
 
-func New(services map[string]func(pimple Pimple)interface{})*Pimple{
+func New(services map[string]func(*Pimple)interface{})*Pimple{
   return &Pimple{services:services}
 }
+
+func (p *Pimple) Value(key string,value interface{})*Pimple{
+  p.services[key] = func(p *Pimple)interface{}{
+    return value
+  }
+  return p
+}
+
+func (p *Pimple) Set(key string,fn func(*Pimple)interface{)}*Pimple{
+  once :=new(sync.Once)
+  p.services[key]= func(p *Pimple)interface{}{
+    var result interface{}
+    once.Do(func(){
+      result = fn(p)
+    })
+    return result
+  }
+  return p
+)
