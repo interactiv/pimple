8b5f533d6b1889bd619d952f5cf2b05cda394415
mparaiso <aikah@free.fr>
2015-04-21 05:11:57 +0200

    Extend

--- pimple.go
+++ pimple.go
@@ -1,46 +1,71 @@
 package pimple
 
-import "sync"
+import (
+	"fmt"
 
-type Pimple struct{
-  services map[string]func()interface{}
+	"sync"
+)
+
+type Pimple struct {
+	services map[string]func() interface{}
 }
 
 // New creates a new container
-func New(services... map[string]func(*Pimple)interface{})(p *Pimple){
-  p= &Pimple{services:map[string]func()interface{}{}}
-  for _,serviceMap:=range services{
-    for key,factory:=range serviceMap{
-      p.Set(key,factory)
-    }
-  }
-  return
+func New(services ...map[string]func(*Pimple) interface{}) (p *Pimple) {
+	p = &Pimple{services: map[string]func() interface{}{}}
+	for _, serviceMap := range services {
+		for key, factory := range serviceMap {
+			p.Set(key, factory)
+		}
+	}
+	return
 }
+
 // Value sets a value
-func (p *Pimple) Value(key string,value interface{})*Pimple{
-  p.services[key] = func()interface{}{
-    return value
-  }
-  return p
+func (p *Pimple) Value(key string, value interface{}) *Pimple {
+	p.services[key] = func() interface{} {
+		return value
+	}
+	return p
 }
+
 // Set sets a factory for a service, mapped by a key.
 // The factory is executed only once
 // and the is cached
-func (p *Pimple) Set(key string,fn func(*Pimple)interface{})*Pimple{
-  once :=new(sync.Once)
-  p.services[key]= func()interface{}{
-    var result interface{}
-    once.Do(func(){
-      result = fn(p)
-    })
-    return result
-  }
-  return p
+func (p *Pimple) Set(key string, fn func(*Pimple) interface{}) *Pimple {
+	once := new(sync.Once)
+	var result interface{}
+	p.services[key] = func() interface{} {
+		once.Do(func() {
+			result = fn(p)
+		})
+		return result
+	}
+	return p
 }
+
 // Get gets a service result by key
-func (p *Pimple) Get(key string)(result interface{}){
-    if p.services[key]!=nil{
-      result = p.services[key]()
-    }
-    return
+func (p *Pimple) Get(key string) (result interface{}) {
+	if p.services[key] != nil {
+		result = p.services[key]()
+	}
+	return
+}
+
+func (p *Pimple) Exists(key string) bool {
+	if p.services[key] != nil {
+		return true
+	}
+	return false
+}
+
+// Extend extends(decorates) a service definition.
+func (p *Pimple) Extend(serviceName string, definition func(interface{}, *Pimple) interface{}) *Pimple {
+	if !p.Exists(serviceName) {
+		panic(fmt.Sprintf("Pimple : Trying to extend service %s that doesn't not exist\n", serviceName))
+	}
+	extendedService := p.services[serviceName]
+	return p.Set(serviceName, func(p *Pimple) interface{} {
+		return definition(extendedService(), p)
+	})
 }
--- pimple_test.go
+++ pimple_test.go
@@ -4,30 +4,36 @@ import "testing"
 import "github.com/interactiv/expect"
 import "github.com/interactiv/pimple"
 
-func TestPimple(t *testing.T){
-  type Foo struct{
-    baz int
-  }
-  type Bar struct{
-    foo *Foo
-  }
-  type Buzz struct{
-    string string
-  }
-  p:=pimple.New(map[string]func(*pimple.Pimple)interface{}{
-    "foo":func(p *pimple.Pimple)interface{}{
-      return &Foo{baz:1}
-    },
-    "bar":func(p *pimple.Pimple)interface{}{
-      return &Bar{foo:p.Get("foo").(*Foo)}
-    },
-  })
-  bar:=p.Get("bar").(*Bar)
-  p.Value("biz","a")
-  p.Set("buzz",func(p *pimple.Pimple)interface{}{
-    return &Buzz{string:p.Get("biz").(string)}
-  })
-  e:=expect.New(t)
-  e.Expect(bar.foo.baz).ToEqual(1)
-  e.Expect(p.Get("buzz").(*Buzz).string).ToEqual("a")
+func TestPimple(t *testing.T) {
+	type Foo struct {
+		baz int
+	}
+	type Bar struct {
+		foo *Foo
+	}
+	type Buzz struct {
+		string string
+		number int
+	}
+	p := pimple.New(map[string]func(*pimple.Pimple) interface{}{
+		"foo": func(p *pimple.Pimple) interface{} {
+			return &Foo{baz: 1}
+		},
+		"bar": func(p *pimple.Pimple) interface{} {
+			return &Bar{foo: p.Get("foo").(*Foo)}
+		},
+	})
+	bar := p.Get("bar").(*Bar)
+	p.Value("biz", "a")
+	p.Set("buzz", func(p *pimple.Pimple) interface{} {
+		return &Buzz{string: p.Get("biz").(string)}
+	})
+	p.Extend("buzz", func(buzz interface{}, p *pimple.Pimple) interface{} {
+		buzz.(*Buzz).number = 23
+		return buzz
+	})
+	e := expect.New(t)
+	e.Expect(bar.foo.baz).ToEqual(1)
+	e.Expect(p.Get("buzz").(*Buzz).string).ToEqual("a")
+	e.Expect(p.Get("buzz").(*Buzz).number).ToEqual(23)
 }
